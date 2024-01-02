58db7ab807ac93d65501d709a5d5b858a9b8e3fe
rthway <rthway@gmail.com>
2024-01-02 16:08:48 +0545

    Working with header.php

--- header.php
+++ header.php
@@ -1,2 +1,15 @@
-<h2>The Header!</h2>
-<hr>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <title><?php bloginfo( 'name' ); ?></title>
+</head>
+
+<body <?php body_class(); ?>>
+
+<header class="site-header">
+    <h1><?php bloginfo( 'name' ); ?></h1>
+    <h4><?php bloginfo( 'description' ); ?></h4>
+</header>
+
