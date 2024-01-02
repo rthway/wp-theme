28fd439d03cd1ec5e018da987eb753f7e56d6ea0
rthway <rthway@gmail.com>
2024-01-02 16:17:03 +0545

    Wrapping the post output with an <article>

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
-<h1>Custom Theme!</h1>
 <?php
+
 get_header();
 
 if ( have_posts() ) :
 	while ( have_posts() ) : the_post(); ?>
 
-        <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
-		<?php the_content() ?>
+        <article class="post">
+            <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+			<?php the_content() ?>
+        </article>
 	
 	<?php endwhile;
 
