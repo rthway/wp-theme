673d18c1b9fa5ebc46f6a493ffb79b41fb2c4c40
rthway <rthway@gmail.com>
2024-01-02 16:03:48 +0545

    Add Code to Output The Post Title and Post Text

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <h1>Custom Theme!</h1>
+<?php
+
+if ( have_posts() ) :
+	while ( have_posts() ) : the_post(); ?>
+
+        <h2><?php the_title() ?></h2>
+		<?php the_content() ?>
+	
+	<?php endwhile;
+
+else :
+	echo '<p>There are no posts!</p>';
+
+endif;
+
+?> 
+ 
