9144a0edf4766357c1ec0a1d94df4b61a018f810
rthway <rthway@gmail.com>
2024-01-03 10:24:28 +0545

    Create A Custom Menu In WordPress

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
     <header class="site-header">
         <h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
         <h4><?php bloginfo( 'description' ); ?></h4>
+        <nav class="navigation-menu">
+			<?php wp_nav_menu() ?>
+        </nav>
     </header> 
-
-
