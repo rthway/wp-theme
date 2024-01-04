14a06d3dd9d21f1170e34758e0c447d565204913
rthway <rthway@gmail.com>
2024-01-04 09:50:30 +0545

    Style that custom menu

--- functions.php
+++ functions.php
@@ -5,3 +5,5 @@ function custom_theme_assets() {
 }
 
 add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
+
+register_nav_menus( [ 'primary' => __( 'Primary Menu' ) ] ); 
--- header.php
+++ header.php
@@ -13,7 +13,7 @@
         <h1><a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a></h1>
         <h4><?php bloginfo( 'description' ); ?></h4>
         <nav class="navigation-menu">
-            <?php $args = [ 'theme_location' => 'primary' ]; ?>
-			<?php wp_nav_menu() ?>
+			<?php $args = [ 'theme_location' => 'primary' ]; ?>
+			<?php wp_nav_menu( $args ) ?>
         </nav>
     </header> 
